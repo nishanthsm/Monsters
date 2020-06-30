bd41fdf3a385f2a1d5c43aa92bfc3211f2eafbb8
nishanthsm <nishanth.sm97@gmail.com>
2020-06-30 14:27:21 +0530

    Updates

---
+++ precache-manifest.cb4f02976aabdc083b941f2d0f8f41d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49c6aefdf86b7abf2bfcd01a9aa9bee0",
+    "url": "/Monsters/index.html"
+  },
+  {
+    "revision": "6f37912e89bff2644892",
+    "url": "/Monsters/static/css/main.4327230f.chunk.css"
+  },
+  {
+    "revision": "f757cf264434f883c100",
+    "url": "/Monsters/static/js/2.fcb612a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters/static/js/2.fcb612a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f37912e89bff2644892",
+    "url": "/Monsters/static/js/main.69ef0ee8.chunk.js"
+  },
+  {
+    "revision": "3bb570dba0886d34cdb3",
+    "url": "/Monsters/static/js/runtime-main.3275fe42.js"
+  }
+]);
